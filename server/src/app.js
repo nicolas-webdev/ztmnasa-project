2c2c7fec12659990d13871ceeed5b32fabcd4b65
Nicolas <nicobsas+webdev@gmail.com>
2021-05-16 13:55:42 +0900

    add morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -11,8 +12,9 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
-app.use(express.json());
+app.use(morgan("combined"));
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
