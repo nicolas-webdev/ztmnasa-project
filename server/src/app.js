7a1d27382baa5f417bd9f4adcecc8c092ae952f4
Nicolas <nicobsas+webdev@gmail.com>
2021-05-16 16:45:11 +0900

    Add post route

--- server/src/app.js
+++ server/src/app.js
@@ -15,11 +15,12 @@ app.use(
 );
 app.use(morgan("combined"));
 
+// app.use(express.urlencoded());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use("/planets", planetsRouter);
+app.use("/launches", launchesRouter);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "Kepler-442 b",
-  customer: ["ZTM", "NASA"],
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      customers: ["Zero to Mastery", "NASA"],
+      flightNumber: latestFlightNumber,
+      success: true,
+      upcoming: true,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
