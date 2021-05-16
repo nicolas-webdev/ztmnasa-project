2baa409c76cf7f10d83cfeac605b0c81082a5ab8
Nicolas <nicobsas+webdev@gmail.com>
2021-05-16 23:31:11 +0900

    http

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 8000;
-const API_URL = process.env.URL || `https://localhost:${PORT}`;
+const API_URL = process.env.URL || `http://localhost:${PORT}`;
 // const API_URL = "https://ztm-nasa.herokuapp.com";
 
 // Load planets and return as JSON.
